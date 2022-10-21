353ea91fbafc52292fefb32169c9dc7cab89d423
michelle <s4mimerg@uni-trier.de>
2022-10-21 05:01:56 +0200

    Strukturelle Änderungen

--- InterneTools/bilevel_db_creator.py
+++ InterneTools/bilevel_db_creator.py
@@ -16,7 +16,7 @@ def addToHtml(json_str: str, infos: dict):
     infos.update(default_dict)
     page_file.close()
     page_file = open(json_str, "w")
-    page_file.write("---\nlayout: default\ntitle: CIBOLib Instance"+infos["Instance"]+"\n---")
+    page_file.write("---\nlayout: ../default\ntitle: CIBOLib Instance"+infos["Instance"]+"\n---")
 
     page_file.write(json2html.convert(json=infos))
     page_file.close()
@@ -27,13 +27,10 @@ def initializeHtml(instance: str, folder: str, collection_home: str, htmls_home:
 
     # get the name of the instance
     instance_without_extension = instance[:-len(".aux")]
+    #where to store html in the end
+    instanceHtml = htmls_home + "/"+instance_without_extension+".html"
 
-    # make the directories if they do not exists and get the new path for the json which will be created by the stats_writer
-    instanceHtml = htmls_home+folder[len(collection_home):]
-    os.makedirs(instanceHtml, exist_ok=True)
-    instanceHtml += "/"+instance_without_extension+".html"
-
-    # let the stats_writer initialize the json of the current instance
+    # let the stats_writer initialize the json of the current instance -> json will change to html
     process = Popen(["./stats_writer", "-i", folder+"/" +
                     instance_without_extension, "-c", instanceHtml])
     exit_code = process.wait()
@@ -43,17 +40,20 @@ def initializeHtml(instance: str, folder: str, collection_home: str, htmls_home:
 def create_htmls_json_andCompress(collection_home: str, htmls_home: str, archives_home: str):
     complete_dictionary: dict = {}
     os.makedirs(archives_home+"archives", exist_ok=True)
+    os.makedirs(htmls_home, exist_ok=True)
     for folder, _, instances in os.walk(collection_home, topdown=True):
         # compress
         if ".git" in folder or folder in collection_home:  # git raus und collection_home raus
             continue
-        tar = tarfile.open(archives_home+"archives/" +
-                           os.path.basename(folder)+".tar.gz", "w:gz")
-        tar.add(folder)
-        tar.close()
+        if True: #muss nicht immer laufen, wenn ich an den Unterseiten arbeite
+            tar = tarfile.open(archives_home+"archives/" +
+                            os.path.basename(folder)+".tar.gz", "w:gz")
+            tar.add(folder)
+            tar.close()
         # create big json and htmls
         for instance in instances:
             if instance.endswith(".aux"):
+                print(instance)
                 instanceHtml, instance_without_extension = initializeHtml(
                     instance, folder, collection_home, htmls_home)
 
@@ -65,7 +65,8 @@ def create_htmls_json_andCompress(collection_home: str, htmls_home: str, archive
 
                 dictionary_of_instance = addToHtml(instanceHtml, {
                     "Instance": instance_without_extension, "Type": type_information, "Class": class_information,
-                    "Path": str("https://cibolib.github.io/htmls/collection/mip-mip/"+folder[len(collection_home+"mip-mip/"):])})
+                    "Path": str("https://cibolib.github.io/htmls/"+instance_without_extension+"./html"),
+                    "Folder":folder[len(collection_home):]})
                 complete_dictionary[instance_without_extension] = dictionary_of_instance
     return complete_dictionary
 
@@ -76,6 +77,6 @@ if __name__ == "__main__":
     archives_home = input("Path for tar.gz ")
     complete_dictionary = create_htmls_json_andCompress(
         collection_home, htmls_home, archives_home)
-    json_file = open(htmls_home+"all_instances.json", "w")
+    json_file = open(htmls_home+"/all_instances.json", "w")
     json.dump(complete_dictionary, json_file)
     json_file.close()
