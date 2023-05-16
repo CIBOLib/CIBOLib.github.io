99a676d9627b4a0cef9866dd27651e8af4b42f47
Johannes Thuerauf <johannes.thuerauf@uni-trier.de>
2023-05-16 22:07:07 +0200

    added mibs single file script and script filmosi parser

---
+++ InterneTools/solution_parser/run_filmosi_parser_executer_for_all_instances_in_collection_server.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import argparse
+"""
+We run filmosi_solution_parser_executer.py for all instances/directories in collection
+insert the paths at ...
+"""
+
+arg_parser = argparse.ArgumentParser(
+    description="run filmosi_solution_parser_executer for each file of collection"
+)
+arg_parser.add_argument(
+    "--collection",
+    action="store",
+    required=True,
+    help="Directory of the considered instances",
+)
+arg_parser.add_argument(
+    "--logfile_dir", action="store", required=True, help="Directory of the logfiles"
+)
+arg_parser.add_argument(
+    "--store_dir", action="store", required=True, help="Directory in which the results are stored"
+)
+
+args = arg_parser.parse_args()
+
+# now we go through all directories of collection
+root_dir = args.collection
+
+# containing all logfiles of filmosi
+logfile_dir = args.logfile_dir
+
+# directory where to store the output
+output_dir = args.store_dir
+
+
+for root, dirs, files in os.walk(root_dir):
+    # exclude hidden directories
+    dirs[:] = [d for d in dirs if not d[0] == '.']
+    # # run filmosi_solution_parser_executer.py for the directories
+    for dir in dirs:
+        command=["python3" ,"filmosi_solution_parser_executer.py",
+                 "--input_mpsaux_dir", root+"/"+str(dir),
+                 "--input_log_dir", logfile_dir,
+                 "--output_dir", output_dir]
+        subprocess.run(command);  #run waits until the process is finished
